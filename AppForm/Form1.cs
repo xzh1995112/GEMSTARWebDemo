6126023a9dab24ba94b3e3aa210cd7864f23bc3f
熊壮 <783898846@qq.com>
2019-11-07 09:25:57 +0800

    新增AppForm解决方案 新增FTP上传TXT文件Demo

---
+++ AppForm/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace AppForm
+{
+	public partial class Form1 : Form
+	{
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		//Main
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var url = "https://openapi.bestsign.info/openapi/v2/storage/contract/download/?developerId=1552635811012161536&rtick=15711111702850&signType=rsa&sign=IVbtnBJ%2BVXBpONZM6VCjRJNPOT1KFcElANfdTYtDvCkA4LJo5dD8IlU%2BMINKCwt1kXLrUixYBdalBynWRVU5XE%2FEQ6nRkYkYDMI1hkgZp277NV4EJ0QbPOeVYVLoJlaIgRbJZ8AZUZANVYyLd9UaMCvCrkq5BtlkBLDSSR%2BoF4wPFJ5U0esx6gE3c%2F59BRNdRpz0LcxKDGHJKfMbvdSryfoyhD9cBT3FA4hB%2F%2F8KYVEQ5IlOF9Spiy5olbK7Pqt3sN3DFWUW82ZZu6DvH8TFYwU5DvyNnaBLwzkyE2Rxij7SiTRP7So1K%2B%2BpbS5jbovyVqE7J4JdAMCd5Hp1H1mwGQ%3D%3D&contractId=156151051001000001";
+			Change(url);
+		}
+
+		public async void Change(string url)
+		{
+			byte[] buffer = await GetStream(url);
+			string text = ExtractTextFromPdf(buffer);
+			this.richTextBox1.Text = text;
+		}
+
+		public static string ExtractTextFromPdf(byte[] data)
+		{
+			using (PdfReader reader = new PdfReader(data))
+			{
+				StringBuilder text = new StringBuilder();
+
+				for (int i = 1; i <= reader.NumberOfPages; i++)
+				{
+					text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+				}
+
+				return text.ToString();
+			}
+		}
+
+		public static string ExtractTextFromPdf(string path)
+		{
+			using (PdfReader reader = new PdfReader(path))
+			{
+				StringBuilder text = new StringBuilder();
+
+				for (int i = 1; i <= reader.NumberOfPages; i++)
+				{
+					text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+				}
+
+				return text.ToString();
+			}
+		}
+
+		#region Tools
+
+		/// <summary>
+		/// 通用方法
+		/// </summary>
+		/// <returns></returns>
+		private async Task<byte[]> GetStream(string url)
+		{
+			return await new HttpClient().GetByteArrayAsync(url);
+		}
+
+		/// <summary>
+		/// 将文件转换成byte[] 数组
+		/// </summary>
+		/// <param name="fileUrl">文件路径文件名称</param>
+		/// <returns>byte[]</returns>
+		protected byte[] GetFileData(string fileUrl)
+		{
+			FileStream fs = new FileStream(fileUrl, FileMode.Open, FileAccess.Read);
+			try
+			{
+				byte[] buffur = new byte[fs.Length];
+				fs.Read(buffur, 0, (int)fs.Length);
+
+				return buffur;
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+			finally
+			{
+				if (fs != null)
+				{
+					//关闭资源
+					fs.Close();
+				}
+			}
+		}
+
+
+		/// <summary>
+		/// 将文件转换成byte[] 数组
+		/// </summary>
+		/// <param name="fileUrl">文件路径文件名称</param>
+		/// <returns>byte[]</returns>
+
+		protected byte[] AuthGetFileData(string fileUrl)
+		{
+			using (FileStream fs = new FileStream(fileUrl, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+			{
+				byte[] buffur = new byte[fs.Length];
+				using (BinaryWriter bw = new BinaryWriter(fs))
+				{
+					bw.Write(buffur);
+					bw.Close();
+				}
+				return buffur;
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ AppForm/Form2.cs
@@ -0,0 +1,86 @@
+﻿using FluentFTP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppForm
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WriteFile();
+            ConnectionFtp("139.9.43.82", 30000, "xzh", "xzh");
+            //ConnectionFtp("10.101.71.147", 30000, "payment", "payment");
+        }
+
+        private void ConnectionFtp(string ip, int port, string user, string password)
+        {
+            try
+            {
+                using (var conn = new FtpClient())
+                {
+                    conn.Host = ip;
+                    conn.Port = port;
+                    conn.Credentials = new NetworkCredential(user, password);
+
+                    conn.EncryptionMode = FtpEncryptionMode.None;
+
+                    conn.SslProtocols = SslProtocols.Default | SslProtocols.Tls11 | SslProtocols.Tls12;
+                    conn.ValidateCertificate += OnValidateCertificate;
+
+                    conn.Connect();
+
+                    if (!conn.UploadFile(Application.StartupPath + "/test.txt", "/test_by_xzh.txt"))
+                    {
+                        MessageBox.Show("上传不成功！", "提示");
+                    }
+
+                    conn.Disconnect();
+
+                    MessageBox.Show("上传成功！", "提示", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void OnValidateCertificate(FtpClient control, FtpSslValidationEventArgs e)
+        {
+            e.Accept = true;
+        }
+
+        //注册
+        public void WriteFile()
+        {
+            string path = Application.StartupPath + "/test.txt";
+
+            FileStream fs = new FileStream(path, FileMode.Append);
+            StreamWriter sr = new StreamWriter(fs);
+            sr.WriteLine(DateTime.Now.Ticks);//开始写入值
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
